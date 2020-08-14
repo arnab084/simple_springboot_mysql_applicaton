28c98987cb9c182abcca0562dbf3f5ec78bdec6a
Arnab Chakraborty <arnab.chakraborty@optimizeitsystems.com>
2020-08-14 20:08:20 +0530

    Updated Swagger

--- initial/src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ initial/src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -1,12 +1,24 @@
 package com.example.accessingdatamysql;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-import com.example.accessingdatamysql.User;
+import java.util.List;
+
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+	
+	
+	List<User> findByEmail(String email);
+	
+	List<User> findByName(String name);
+	
+	@Query("SELECT u FROM User u where u.name = :name") 
+    List<User> findIdByName(@Param("name") String name);
+	
 
 }
