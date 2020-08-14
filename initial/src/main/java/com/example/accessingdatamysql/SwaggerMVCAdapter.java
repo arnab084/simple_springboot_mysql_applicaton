d84c7ed594e2ba2f9b62b1a670ad1546525a5c75
Arnab Chakraborty <arnab.chakraborty@optimizeitsystems.com>
2020-08-14 20:08:34 +0530

    Updated swagger

---
+++ initial/src/main/java/com/example/accessingdatamysql/SwaggerMVCAdapter.java
@@ -0,0 +1,19 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+public class SwaggerMVCAdapter extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+}
